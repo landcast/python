8dcb6c631ac892f049ac3adf0a3914e97a492f9c
litao <tao.li02@regiglobal.com>
2018-03-02 15:42:06 +0800

    add setUp and tearDown()

--- pybdemo/src/unittest/python/helloworld_tests.py
+++ pybdemo/src/unittest/python/helloworld_tests.py
@@ -5,9 +5,14 @@ from helloworld import helloworld
 
 
 class HelloWorldTest(unittest.TestCase):
+
+    def setUp(self):
+        print('overriding unittest.setUp() method')
+
+    def tearDown(self):
+        print('overriding unittest.tearDown() method')
+
     def test_should_issue_hello_world_message(self):
         out = mock()
-
         helloworld(out)
-
         verify(out).write("Hello world of Python\n")
