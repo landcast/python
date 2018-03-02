c0f95b6dc6175c597e677e4f6f52c6fb9f0c3ddc
litao <tao.li02@regiglobal.com>
2018-03-02 14:41:42 +0800

    temp

--- pybdemo/build.py
+++ pybdemo/build.py
@@ -2,9 +2,11 @@ from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
 use_plugin("python.unittest")
+use_plugin("pypi:pybuilder_pytest")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
+use_plugin('pypi:pybuilder_pytest_coverage')
 use_plugin("python.distutils")
 
 
@@ -15,3 +17,6 @@ default_task = "publish"
 @init
 def initialize(project):
     project.build_depends_on('mockito')
+    project.get_property('pytest_extra_args').append('-x')
+    project.set_property("coverage_break_build", False)
+    project.set_property_if_unset("pytest_coverage_break_build_threshold", 30)
