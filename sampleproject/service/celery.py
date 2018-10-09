965e1b93b6c2d6797c21c1fe42d4158a358d1209
litao <tao.li02@regiglobal.com>
2018-10-09 16:31:30 +0800

    temp fix for 3.6.5 not support 3.7.0 due to use reserved keyword async for internal module name

--- sampleproject/service/celery.py
+++ sampleproject/service/celery.py
@@ -2,10 +2,10 @@ from __future__ import absolute_import, unicode_literals
 from celery import Celery
 
 
-app = Celery('proj',
-             broker='redis://:@localhost:6379/0',
-             backend='redis://:@localhost:6379/1',
-             include=['proj.tasks'])
+app = Celery('service',
+             broker='redis://localhost:6379/0',
+             backend='redis://localhost:6379/1',
+             include=['service.tasks'])
 
 
 # Optional configuration, see the application user guide.
